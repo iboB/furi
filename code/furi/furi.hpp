73c2ad38fbeff13f09fb8c22e544aaff52015cc7
Borislav Stanimirov <b.stanimirov@abv.bg>
2022-03-16 08:35:28 +0200

    C++ wrapper

--- code/furi/furi.hpp
+++ code/furi/furi.hpp
@@ -9,6 +9,7 @@
 #include <cstring>
 #include <cassert>
 #include <string_view>
+#include <utility>
 
 namespace furi
 {
@@ -30,6 +31,7 @@ public:
         : m_sv(capi::furi_make_sv_from_string(cstring)) {}
     opt_string_view(std::string_view str) noexcept // intentionally implicit
         : m_sv(capi::furi_make_sv(str.data(), str.data() + str.size())) {}
+    explicit opt_string_view(const capi::furi_sv& sv) : m_sv(sv) {}
     opt_string_view(const char* begin, const char* end) noexcept
         : m_sv(capi::furi_make_sv(begin, end)) {}
     template <typename CI> // contiguous iterator
@@ -53,6 +55,221 @@ public:
     // rely on implicit casts for these
     friend bool operator==(const char* a, opt_string_view b) noexcept { return b == a; }
     friend bool operator!=(const char* a, opt_string_view b) noexcept { return b != a; }
+};
+
+struct uri_split
+{
+    opt_string_view scheme;
+    opt_string_view authority;
+    opt_string_view path;
+    opt_string_view query;
+    opt_string_view fragment;
+
+    static uri_split from_capi(const capi::furi_uri_split& cs) noexcept
+    {
+        return {
+            opt_string_view(cs.scheme),
+            opt_string_view(cs.authority),
+            opt_string_view(cs.path),
+            opt_string_view(cs.query),
+            opt_string_view(cs.fragment),
+        };
+    }
+
+    static uri_split from_uri(opt_string_view u) noexcept
+    {
+        return from_capi(capi::furi_split_uri(u.c_sv()));
+    }
+
+    static opt_string_view get_scheme_from_uri(opt_string_view u) noexcept
+    {
+        return opt_string_view(capi::furi_get_scheme_from_uri(u.c_sv()));
+    }
+
+    static opt_string_view get_authority_from_uri(opt_string_view u) noexcept
+    {
+        return opt_string_view(capi::furi_get_authority_from_uri(u.c_sv()));
+    }
+
+    static opt_string_view get_path_from_uri(opt_string_view u) noexcept
+    {
+        return opt_string_view(capi::furi_get_path_from_uri(u.c_sv()));
+    }
+
+    static opt_string_view get_query_from_uri(opt_string_view u) noexcept
+    {
+        return opt_string_view(capi::furi_get_query_from_uri(u.c_sv()));
+    }
+
+    static opt_string_view get_fragment_from_uri(opt_string_view u) noexcept
+    {
+        return opt_string_view(capi::furi_get_fragment_from_uri(u.c_sv()));
+    }
+};
+
+struct authority_split
+{
+    opt_string_view userinfo;
+    opt_string_view host;
+    opt_string_view port;
+
+    static authority_split from_capi(const capi::furi_authority_split& as) noexcept
+    {
+        return {
+            opt_string_view(as.userinfo),
+            opt_string_view(as.host),
+            opt_string_view(as.port),
+        };
+    }
+
+    static authority_split from_authority(opt_string_view a) noexcept
+    {
+        return from_capi(capi::furi_split_authority(a.c_sv()));
+    }
+
+    static opt_string_view get_userinfo_from_authority(opt_string_view a) noexcept
+    {
+        return opt_string_view(capi::furi_get_userinfo_from_authority(a.c_sv()));
+    }
+
+    static opt_string_view get_host_from_authority(opt_string_view a) noexcept
+    {
+        return opt_string_view(capi::furi_get_host_from_authority(a.c_sv()));
+    }
+
+    static opt_string_view get_port_from_authority(opt_string_view a) noexcept
+    {
+        return opt_string_view(capi::furi_get_port_from_authority(a.c_sv()));
+    }
+};
+
+struct userinfo_split
+{
+    opt_string_view username;
+    opt_string_view password;
+
+    static userinfo_split from_capi(const capi::furi_userinfo_split& uis) noexcept
+    {
+        return {
+            opt_string_view(uis.username),
+            opt_string_view(uis.password),
+        };
+    }
+
+    static userinfo_split from_userinfo(opt_string_view ui) noexcept
+    {
+        return from_capi(capi::furi_split_userinfo(ui.c_sv()));
+    }
+
+    static opt_string_view get_username_from_userinfo(opt_string_view ui) noexcept
+    {
+        return opt_string_view(capi::furi_get_username_from_userinfo(ui.c_sv()));
+    }
+
+    static opt_string_view get_password_from_userinfo(opt_string_view ui) noexcept
+    {
+        return opt_string_view(capi::furi_get_password_from_userinfo(ui.c_sv()));
+    }
+};
+
+class path_iterator
+{
+    capi::furi_path_iter m_pi;
+public:
+    path_iterator() : m_pi({ nullptr, nullptr, nullptr }) {}
+    explicit path_iterator(const capi::furi_path_iter& pi) : m_pi(pi) {};
+
+    static path_iterator begin_of(opt_string_view path) noexcept
+    {
+        return path_iterator(capi::furi_make_path_iter_begin(path.c_sv()));
+    }
+
+    static path_iterator end_of(opt_string_view path) noexcept
+    {
+        return path_iterator(capi::furi_make_path_iter_end(path.c_sv()));
+    }
+
+    void operator++() noexcept
+    {
+        capi::furi_path_iter_next(&m_pi);
+    }
+
+    opt_string_view operator*() const noexcept
+    {
+        return opt_string_view(capi::furi_path_iter_get_value(m_pi));
+    }
+
+    bool operator==(const path_iterator& other) const noexcept
+    {
+        return capi::furi_path_iter_equal(m_pi, other.m_pi);
+    }
+
+    bool operator!=(const path_iterator& other) const noexcept
+    {
+        return !capi::furi_path_iter_equal(m_pi, other.m_pi);
+    }
+};
+
+struct path_view : public opt_string_view
+{
+public:
+    using opt_string_view::opt_string_view;
+    using const_iterator = path_iterator;
+    const_iterator begin() const noexcept { return const_iterator::begin_of(*this); }
+    const_iterator end() const noexcept { return const_iterator::end_of(*this); }
+};
+
+using query_item = std::pair<opt_string_view, opt_string_view>;
+
+class query_iterator
+{
+    capi::furi_query_iter m_qi;
+public:
+    query_iterator() : m_qi({ nullptr, nullptr, nullptr }) {}
+    explicit query_iterator(const capi::furi_query_iter& pi) : m_qi(pi) {};
+
+    static query_iterator begin_of(opt_string_view path) noexcept
+    {
+        return query_iterator(capi::furi_make_query_iter_begin(path.c_sv()));
+    }
+
+    static query_iterator end_of(opt_string_view path) noexcept
+    {
+        return query_iterator(capi::furi_make_query_iter_end(path.c_sv()));
+    }
+
+    void operator++() noexcept
+    {
+        capi::furi_query_iter_next(&m_qi);
+    }
+
+    query_item operator*() const noexcept
+    {
+        auto c = furi_query_iter_get_value(m_qi);
+        return {
+            opt_string_view(c.key),
+            opt_string_view(c.value),
+        };
+    }
+
+    bool operator==(const query_iterator& other) const noexcept
+    {
+        return capi::furi_query_iter_equal(m_qi, other.m_qi);
+    }
+
+    bool operator!=(const query_iterator& other) const noexcept
+    {
+        return !capi::furi_query_iter_equal(m_qi, other.m_qi);
+    }
+};
+
+struct query_view : public opt_string_view
+{
+public:
+    using opt_string_view::opt_string_view;
+    using const_iterator = query_iterator;
+    const_iterator begin() const noexcept { return const_iterator::begin_of(*this); }
+    const_iterator end() const noexcept { return const_iterator::end_of(*this); }
 };
 
 }
--- test/t-furi.cpp
+++ test/t-furi.cpp
@@ -8,7 +8,12 @@
 #include <doctest/doctest.h>
 #include <furi/furi.hpp>
 
+#include <vector>
+
 using namespace furi;
+
+// the c api is extensively tested
+// this test suite has only basic tests to check that the C++ wrapper has done its job
 
 TEST_SUITE_BEGIN("furi");
 
@@ -60,4 +65,87 @@ TEST_CASE("string view")
     CHECK(abc != "xyz");
     CHECK(abc.data() != nullptr);
     std_sv_test(abc);
+}
+
+TEST_CASE("uri_split")
+{
+    std::string_view uri = "http://x.com:43/abc?xyz#top";
+    auto s = uri_split::from_uri(uri);
+    CHECK(s.scheme == "http");
+    CHECK(s.scheme == uri_split::get_scheme_from_uri(uri));
+    CHECK(s.authority == "x.com:43");
+    CHECK(s.authority == uri_split::get_authority_from_uri(uri));
+    CHECK(s.path == "/abc");
+    CHECK(s.path == uri_split::get_path_from_uri(uri));
+    CHECK(s.query == "xyz");
+    CHECK(s.query == uri_split::get_query_from_uri(uri));
+    CHECK(s.fragment == "top");
+    CHECK(s.fragment == uri_split::get_fragment_from_uri(uri));
+
+    s = uri_split::from_uri("");
+    CHECK_FALSE(s.scheme);
+    CHECK_FALSE(s.authority);
+    CHECK(bool(s.path));
+    CHECK(s.path.empty());
+    CHECK_FALSE(s.query);
+    CHECK_FALSE(s.fragment);
+}
+
+TEST_CASE("authority_split")
+{
+    auto authority = "alice:pass@foo.com:44";
+    auto s = authority_split::from_authority(authority);
+    CHECK(s.userinfo == "alice:pass");
+    CHECK(s.userinfo == authority_split::get_userinfo_from_authority(authority));
+    CHECK(s.host == "foo.com");
+    CHECK(s.host == authority_split::get_host_from_authority(authority));
+    CHECK(s.port == "44");
+    CHECK(s.port == authority_split::get_port_from_authority(authority));
+
+    s = authority_split::from_authority("");
+    CHECK_FALSE(s.userinfo);
+    CHECK(bool(s.host));
+    CHECK(s.host.empty());
+    CHECK_FALSE(s.port);
+}
+
+TEST_CASE("userinfo_split")
+{
+    auto ui = "user:pass";
+    auto s = userinfo_split::from_userinfo(ui);
+    CHECK(s.username == "user");
+    CHECK(s.username == userinfo_split::get_username_from_userinfo(ui));
+    CHECK(s.password == "pass");
+    CHECK(s.password == userinfo_split::get_password_from_userinfo(ui));
+
+    CHECK(userinfo_split::get_username_from_userinfo("user") == "user");
+    CHECK_FALSE(userinfo_split::get_password_from_userinfo("user"));
+}
+
+TEST_CASE("path_view/iter")
+{
+    std::vector<std::string_view> vec = {"foo", "bar", "baz", ""};
+
+    path_view pv = "/foo/bar/baz/";
+    std::vector<std::string_view> check;
+    for (auto e : pv)
+    {
+        check.push_back(e);
+    }
+
+    CHECK(vec == check);
+}
+
+TEST_CASE("query_view/iter")
+{
+    std::vector<query_item> vec = {{"xy", "23"}, {"f", {}}, {"q", "z"}};
+
+    query_view qv = "xy=23&f&q=z";
+    std::vector<query_item> check;
+    for (auto e : qv)
+    {
+        check.push_back(e);
+    }
+
+    CHECK(vec == check);
 }
